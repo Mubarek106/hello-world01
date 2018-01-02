79e69126c9e2ae278df8dfee03ef79fbc83aafb5
Mubarek106 <34705492+Mubarek106@users.noreply.github.com>
2018-01-02 20:16:17 +0300

    Update takeBreaks.py

--- takeBreaks.py
+++ takeBreaks.py
@@ -1,1 +1,12 @@
+# The program runs every thirty minutes
+import time
+import webbrowser
 
+total_breaks = 3
+break_count = 0
+
+print("This program started on" + time.ctime())
+while(break_count < total_breaks):
+    time.sleep(2*60*30) 
+    webbrowser.open("https://www.youtube.com/watch?v=5VQi8kbtiLU")
+    break_count = break_count + 1  #break_count += 1
